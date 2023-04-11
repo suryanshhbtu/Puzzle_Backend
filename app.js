1ef991bc460a121037aa75169b6a10f4615eabf9
harcourtian <suryanshhbtu@gmail.com>
2023-04-11 21:48:01 +0530

    added user model, morgan, body-parser

--- app.js
+++ app.js
@@ -2,9 +2,16 @@
 const express = require('express'); // importing express
 
 const app = express();              // executing express
+const morgan = require('morgan');   // to log every request in terminal for developer prospective
+const bodyParser = require('body-parser'); // befor it req.body -> undefined
 
+// to use morgan
+app.use(morgan('dev'));
 
+// type of data that can be parsed
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 // Handling CORS Errors -> Cross-Origin-Resourse-Sharing
 app.use((req, res, next) => {
@@ -16,15 +23,16 @@ app.use((req, res, next) => {
     }
     next();
 });
-
-app.use((error, req, res, next) => {       // ERROR INDORMATION INSIDE HTML
+app.use((req, res, next) =>{
+    const error = new Error('Not Found');
+    error.status = 404
+    next(error);
+});
+app.use((error, req, res, next) => {       // ERROR INFORMATION INSIDE HTML into JSON
     res.status(error.status || 500);
     res.json({
-        error: {
-            text: "Some Error Occured",
-            message: error.message
-        }
-    })
+        message: error.message
+    });
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -5,3 +5,11 @@ const port = process.env.PORT || 3030;  // defining port or by environment varia
 
 const server = http.createServer(app);     
 server.listen(port);
+/*
+Node.js http.createServer(requestListener)
+    1) turns you pc into HTTP server,
+    2) creates an HTTP Server Object,
+    3) can listen to Ports on your computer 
+        and execute a function, a requestListner(PARAMETER), 
+        each time when request is made.
+*/
